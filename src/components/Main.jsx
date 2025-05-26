3d5ae5abaa1a284ddfc069b246e98077560559dd
danirando <danielerando@tiscali.it>
2025-05-26 14:02:38 +0200

    add titles list

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,3 +1,18 @@
+import React, { useState } from "react";
+
 export default function Main() {
-  return <main></main>;
+  const [postsTitle, setPostsTitle] = useState(["Post 1", "Post 2", "Post 3"]);
+  return (
+    <main>
+      <div className="container mt-5">
+        <ul className="list-group mb-4">
+          {postsTitle.map((title, i) => (
+            <li key={i} className="list-group-item">
+              {title}
+            </li>
+          ))}
+        </ul>
+      </div>
+    </main>
+  );
 }
