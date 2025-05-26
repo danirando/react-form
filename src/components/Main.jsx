6e2864c7e9109c892d26e1a39805cbfe5fceee47
danirando <danielerando@tiscali.it>
2025-05-26 14:13:40 +0200

    add logic form submit

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -3,8 +3,16 @@ import React, { useState } from "react";
 export default function Main() {
   const [postsTitle, setPostsTitle] = useState(["Post 1", "Post 2", "Post 3"]);
 
+  const [inputValue, setInputValue] = useState("");
+
   const handleFormSubmit = (e) => {
     e.preventDefault();
+    setPostsTitle([...postsTitle, inputValue]);
+    setInputValue("");
+  };
+
+  const handleInputChange = (e) => {
+    setInputValue(e.target.value);
   };
 
   return (
@@ -18,7 +26,7 @@ export default function Main() {
           ))}
         </ul>
         <form onSubmit={handleFormSubmit}>
-          <input type="text" value={""} />
+          <input type="text" value={inputValue} onChange={handleInputChange} />
           <button className="btn btn-primary">Submit</button>
         </form>
       </div>
