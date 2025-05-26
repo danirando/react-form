dc83c2e3eeac80cb8d97c339e323ac8e2d886d6e
danirando <danielerando@tiscali.it>
2025-05-26 14:41:24 +0200

    add remove button

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,9 +1,15 @@
 import React, { useState } from "react";
 
+import trashIcon from "../assets/img/trash.svg";
+
 export default function Main() {
   const [postsTitle, setPostsTitle] = useState(["Post 1", "Post 2", "Post 3"]);
 
   const [inputValue, setInputValue] = useState("");
+
+  const handleDeletePost = (iToRemove) => {
+    setPostsTitle(postsTitle.filter((postTitle, i) => i !== iToRemove));
+  };
 
   const handleFormSubmit = (e) => {
     e.preventDefault();
@@ -20,12 +26,21 @@ export default function Main() {
       <div className="container mt-5">
         <ul className="list-group mb-4">
           {postsTitle.map((title, i) => (
-            <li key={i} className="list-group-item">
-              {title}
+            <li
+              key={i}
+              className="list-group-item d-flex align-items-center justify-content-between">
+              {title}{" "}
+              <div className="trash-container">
+                <button
+                  className="btn btn-danger"
+                  onClick={() => handleDeletePost(i)}>
+                  <img src={trashIcon} alt="" />
+                </button>
+              </div>
             </li>
           ))}
         </ul>
-        <form onSubmit={handleFormSubmit}>
+        <form onSubmit={handleFormSubmit} className="d-flex gap-4">
           <input type="text" value={inputValue} onChange={handleInputChange} />
           <button className="btn btn-primary">Submit</button>
         </form>
