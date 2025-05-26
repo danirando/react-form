e0e089c13e238748ef27c9c354cd81c1659e1672
danirando <danielerando@tiscali.it>
2025-05-26 14:06:07 +0200

    add form

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -2,6 +2,11 @@ import React, { useState } from "react";
 
 export default function Main() {
   const [postsTitle, setPostsTitle] = useState(["Post 1", "Post 2", "Post 3"]);
+
+  const handleFormSubmit = (e) => {
+    e.preventDefault();
+  };
+
   return (
     <main>
       <div className="container mt-5">
@@ -12,6 +17,10 @@ export default function Main() {
             </li>
           ))}
         </ul>
+        <form onSubmit={handleFormSubmit}>
+          <input type="text" value={""} />
+          <button className="btn btn-primary">Submit</button>
+        </form>
       </div>
     </main>
   );
